8cc4d62712ab772638b8c92369915c5f01c8f385
Daniel Alvarez <alvarezd@kapsch.net>
2017-09-20 08:16:58 -0300

    initial commit, from original zip file

---
+++ id_tokens/Api/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace JavaScriptImplicitClient_Simple.Api
+{
+    [Route("api/test")]
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var cp = User as ClaimsPrincipal;
+            
+            var claims = new Dictionary<string, object>();
+            var groups =
+                from c in cp.Claims
+                group c by c.Type into g
+                select g;
+            foreach(var item in groups)
+            {
+                if (item.Count() > 1)
+                {
+                    claims.Add(item.Key, item.Select(x=>x.Value).ToArray());
+                }
+                else
+                {
+                    claims.Add(item.Key, item.First().Value);
+                }
+            }
+            
+            return Json(new { message = "API called", access_token_claims=claims });
+        }
+    }
+}
---
+++ id_tokens/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(JavaScriptImplicitClient_Simple.Startup))]
+
+namespace JavaScriptImplicitClient_Simple
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseIdentityServerBearerTokenAuthentication(new Thinktecture.IdentityServer.AccessTokenValidation.IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "https://localhost:44333/core",
+                RequiredScopes = new string[] { "api1" }
+            });
+
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Filters.Add(new AuthorizeAttribute());
+            app.UseWebApi(config);
+        }
+    }
+}
